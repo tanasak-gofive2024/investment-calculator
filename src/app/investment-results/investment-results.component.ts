8f64824b1060b24292e11610522c79ee06a51e42
tanasak-gofive2024 <tanasak.p@gofive.co.th>
2024-08-28 18:20:10 +0700

    investment calculator

---
+++ src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,13 @@
+import { Component, computed } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  constructor(private readonly investmentService: InvestmentService) {}
+
+  results = computed(() => this.investmentService.resultData())
+}
